fee93254b32edb82eafbcbe1b3dc9606f7c62e7e
Andrian <traian-007@mail.ru>
2023-05-13 00:22:45 +0300

    Handle global errors via error endpoint

--- Orion.API/Filters/ErrorHandlingFilterAttribute.cs
+++ Orion.API/Filters/ErrorHandlingFilterAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
 
 namespace Orion.API.Filters
 {
@@ -14,10 +15,14 @@ namespace Orion.API.Filters
 
             var exception = context.Exception;
 
-            context.Result = new ObjectResult(new { error = "An error occurred while processing your request. " })
+            var problemDetails = new ProblemDetails
             {
-                StatusCode = 500
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+                Title = "An error occurred while processing your request. ",
+                Status = (int)HttpStatusCode.InternalServerError
             };
+
+            context.Result = new ObjectResult(problemDetails);
 
             context.ExceptionHandled = true;
         }
--- Orion.API/Program.cs
+++ Orion.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Orion.API.Errors;
 using Orion.API.Filters;
 using Orion.Application;
 using Orion.Ifrastructure;
@@ -8,12 +10,23 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
-    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, OrionProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
-   /* app.UseMiddleware<ErrorHandlingMiddleware>();*/
+    /* app.UseMiddleware<ErrorHandlingMiddleware>();*/
+    app.UseExceptionHandler("/error");
+
+ /*   app.Map("/error", (HttpContext httpContext) =>
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        return Results.Problem();
+    });*/
 
     app.UseHttpsRedirection();
 
