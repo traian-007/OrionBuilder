c03585bced9c4174d826747a96e2a6f3592dd8d4
Andrian <traian-007@mail.ru>
2023-05-12 16:07:06 +0300

    Create UserRepository interface and User entity

---
+++ DomainModel.md
@@ -0,0 +1,14 @@
+# Domain Models
+
+## User
+
+```json
+{
+    "id": "0000000000-00000000-000-0000000",
+    "firstName": "Andrian",
+    "lastName": "Gaidarji",
+    "email": "mai@mail.com",
+    "password": "milion"
+}
+```
+> Note: Passwords shouldn't be laying around this
--- Orion.Application/Services/Authentication/AuthenticationService.cs
+++ Orion.Application/Services/Authentication/AuthenticationService.cs
@@ -1,43 +1,70 @@
 ﻿using Orion.Application.Common.Interfaces.Authentication;
+using Orion.Application.Common.Interfaces.Persistence;
+using Orion.Domain.Entities;
 
 namespace Orion.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
-
-        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        private readonly IUserRepository _userRepository;
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
         {
             _jwtTokenGenerator = jwtTokenGenerator;
-        }
-
-        public AuthenticationResult Login(string email, string password)
-        {
-     
-
-            return new AuthenticationResult(
-                Guid.NewGuid(),
-                "firstName",
-                "LastName",
-                email,
-                "toke");
+            _userRepository = userRepository;
         }
 
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
-            //Check if user already exists
+            // 1. Validate the user doesn't exist
+            if(_userRepository.GetUserByEmail(email) is not null)
+            {
+                throw new Exception("User with given email already exist!");
+            }
 
-            //Create user (generate a unique ID)
+            // 2. Create user (generate a unique ID) & Persist to DB
+            var user = new User { 
+                FirstName = firstName, 
+                LastName = lastName, 
+                Email = email, 
+                Password = password 
+            };
 
-            //Create JWT token
-            Guid userId = Guid.NewGuid();
+            _userRepository.Add(user);
 
-            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+            // 3. Create JWT token
+
+            var token = _jwtTokenGenerator.GenerateToken(user.Id, firstName, lastName);
 
             return new AuthenticationResult(
-                userId,
+                user.Id,
                 firstName,
                 lastName,
+                email,
+                token);
+        }
+
+        public AuthenticationResult Login(string email, string password)
+        {
+            // 1. Validation the user exists
+            if(_userRepository.GetUserByEmail(email) is not User user)
+            {
+                throw new Exception("User with given email does not exist!");
+            }
+            // 2. Validate the password is correct
+            if (user.Password != password)
+            {
+                throw new Exception("Invalid password");
+            }
+
+            // 3. Create JWT token
+
+            var token = _jwtTokenGenerator.GenerateToken(user.Id, user.FirstName, user.LastName);
+
+            return new AuthenticationResult(
+                user.Id,
+                user.FirstName,
+                user.LastName,
                 email,
                 token);
         }
